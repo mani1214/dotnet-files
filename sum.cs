729ef0e45590add485f95f7edf4bb922a006f241
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-06 14:25:17 +0530

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,13 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+         int num = 54;
+            Console.WriteLine(num%10);
+            Console.WriteLine(num/10);
+            Console.WriteLine(num%10+num/10);
+        }
+    }
