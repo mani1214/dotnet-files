bd479f506e0594677186aebe37ac1aa1b0806e85
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-02-10 11:01:05 +0530

    Add files via upload

---
+++ LuckyNumber.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using System;
+
+class Program
+{ 
+    public static int GetLuckyNumber(string Dob) 
+        {
+            string[] res = Dob.Split('-');
+            int date = int.Parse(res[0]);
+            int mon = ConvertMonToNum(res[1]);
+            int year = int.Parse(res[2]);
+            int sum = date + mon + year;
+            while (sum > 9)
+            {
+                sum = GetSumOfDigits(sum);
+            }
+            return sum;
+
+        }
+
+        public static int ConvertMonToNum(string month) 
+        {
+            month = month.ToLower();
+            string[] months = {"jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec" };
+            for (int i = 0; i < months.Length; i++)
+            {
+                if(month.Contains(months[i]))
+                {
+                    return i + 1;
+                }
+            }
+            return 0;
+        
+        }
+
+        public static int GetSumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum = sum + digit;
+                num = num / 10;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLuckyNumber("18--1996"));
+            Console.WriteLine(ConvertMonToNum("January"));
+            Console.WriteLine(GetSumOfDigits(123));
+        }
+    }
